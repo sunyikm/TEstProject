a3964fce857f403c187ef98f6c525d8a91f42004
yuxing <158487926+sunyikm@users.noreply.github.com>
2024-11-23 23:44:01 +0100

    Update TEstProject.dll and TEstProject.exe

---
+++ readability.cs
@@ -0,0 +1,24 @@
+/*
+    This code is a simple example of how to reverse a string 
+    and count the number of times a letter appears in it.
+*/
+
+// string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+// char[] message = originalMessage.ToCharArray();
+// Array.Reverse(message);
+
+// int letterCount=0;
+
+// foreach(char letter in message){
+//     if(letter =='o')
+//     {
+//         letterCount++;
+//     }
+// }
+
+// string newMessage = new string(message);
+
+// Console.WriteLine(newMessage);
+// Console.WriteLine($"'o' appears {letterCount} times");
+
